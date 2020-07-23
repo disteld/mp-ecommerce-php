d5de7b18956b7acb052dec3b43d519f9ebbdf221
disteld <68667458+disteld@users.noreply.github.com>
2020-07-23 20:07:50 -0300

    Add files via upload

--- detail.php
+++ detail.php
@@ -43,9 +43,9 @@
 
 <body class="as-theme-light-heroimage">
   <?php
-
-  //  include_once 'vendor/autoload.php';  //Aqui coloca la ruta en donde descargaste el sdk de mercadopago
-  // require __DIR__ .  '/vendor/autoload.php';
+/*
+    //include_once __DIR__ . '/vendor/autoload.php';  //Aqui coloca la ruta en donde descargaste el sdk de mercadopago
+    require __DIR__ .  '/vendor/autoload.php';
    MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'); // Ya que vas a hacer pruebas de pago, aqui tu access token de prueba, luego puedes agregar el token de produccion
 MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
 
@@ -110,7 +110,7 @@ MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
   echo "\n";
   
   echo "PaymentId: " . $preference->id . "\n";
-
+*/
     ?>
     
     <div class="stack">
