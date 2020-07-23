1824efb4410b772d50acb03f5203ab6be38c0410
disteld <68667458+disteld@users.noreply.github.com>
2020-07-22 21:53:32 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -44,7 +44,7 @@
 <body class="as-theme-light-heroimage">
   <?php
 
-    //include_once 'vendor/autoload.php';  //Aqui coloca la ruta en donde descargaste el sdk de mercadopago
+    include_once __DIR__ . '/vendor/autoload.php';  //Aqui coloca la ruta en donde descargaste el sdk de mercadopago
   /*  require __DIR__ .  '/vendor/autoload.php';
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'); // Ya que vas a hacer pruebas de pago, aqui tu access token de prueba, luego puedes agregar el token de produccion
 
