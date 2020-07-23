9433163e7775882a16a0849171c5f36f1459b485
disteld <68667458+disteld@users.noreply.github.com>
2020-07-22 21:33:31 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -42,6 +42,21 @@
 
 
 <body class="as-theme-light-heroimage">
+<?php 
+ MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'); 
+
+    // Crea un objeto de preferencia
+    $preference = new MercadoPago\Preference();
+
+    // Crea un ítem en la preferencia
+    $item = new MercadoPago\Item();
+    $item->title = 'Producto 1';
+    $item->quantity = 1;
+    $item->unit_price = 300; //Detalle aca, si tu previamente tienes configurado en tu cuenta que eres de algun pais que no maneje decimales en el valor, el valor debe ser entero, sino mercadopago arrojara error
+    $preference->items = array($item);
+    $preference->save();
+
+    ?>
 
     <div class="stack">
         
@@ -130,7 +145,13 @@
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
+                                    <!--<button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>-->
+									<form action="/procesar-pago" method="POST">
+									  <script
+									   src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+									   data-preference-id="<?php echo $preference->id; ?>">
+									  </script>
+									</form>
                                 </div>
                             </div>
                         </div>
