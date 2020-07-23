ff3edf8863e24e848b8272512556c800f8df0301
disteld <68667458+disteld@users.noreply.github.com>
2020-07-23 20:58:34 -0300

    Update create_preference.php

--- create_preference.php
+++ create_preference.php
@@ -1,69 +1,54 @@
 <?php
-	// SDK de Mercado Pago
 	require __DIR__ .  '/vendor/autoload.php';
-
-	// URL
 	$basedir = 'https://' . dirname($_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
-
-	// Agrega credenciales
-	MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-090914-5c508e1b02a34fcce879a999574cf5c9-469485398');
-
-	// Crea un objeto de preferencia
+	MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+        MercadoPago\SDK::setIntegratorId('dev_24c65fb163bf11ea96500242ac130004');
 	$preference = new MercadoPago\Preference();
 
-	// Crea un ítem en la preferencia
-	$item = new MercadoPago\Item();
-	$item->id = "1234";
-	$item->title = $_POST['title'];
-	$item->description = "Dispositivo móvil de Tienda e-commerce";
-	$item->picture_url = $basedir . '/' . str_replace('./', '', $_POST['img']);
-	$item->quantity = intval($_POST['unit']);
-	$item->unit_price = floatval($_POST['price']);
-	$preference->items = array($item);
+	// Crea un objeto de preferencia
+    $preference = new MercadoPago\Preference();
 
-	// Crear payer
-	$payer = new MercadoPago\Payer();
-	$payer->name = "Lalo";
-	$payer->surname = "Landa";
-	$payer->email = "test_user_63274575@testuser.com";
-	$payer->phone = array(
-		"area_code" => "011",
-		"number" => "22223333"
-	);
-	$payer->identification = array(
-		"type" => "DNI",
-		"number" => "22333444"
-	);
-	$payer->address = array(
-		"street_name" => "Falsa",
-		"street_number" => 123,
-		"zip_code" => "1111"
-	);
-	$preference->payer = $payer;
-
-	// Excluir pagos
-	$preference->payment_methods = array(
-		'excluded_payment_methods'=>array(
-			array('id'=>'amex'),
-		),
-		'excluded_payment_types'=>array(
-			array('id'=>'atm'),
-		),
-		'installments'=>6
-	);
-
-	// Back urls
-	$preference->back_urls = array(
-		'failure'=>"$basedir/failure.php",
-		'pending'=>"$basedir/pending.php",
-		'success'=>"$basedir/success.php"
-	);
-
-	// Notification URL
-	$preference->notification_url = "$basedir/log.php";
+    // Crea un ítem en la preferencia
+    $item = new MercadoPago\Item();
+    $item->id = "1234";
+    $item->title = $_POST['title'];
+    $item->quantity = 1;
+    $item->picture_url = $_POST['img'];
+    $item->unit_price = $_POST['price']; //Detalle aca, si tu previamente tienes configurado en tu cuenta que eres de algun pais que no maneje decimales en el valor, el valor debe ser entero, sino mercadopago arrojara error
+    $preference->items = array($item);
 	
-	// Configurar preferencia
-	$preference->auto_return = "approved";
-	$preference->external_reference = "ABCD1234";
+	$preference->payment_methods = array(
+  "excluded_payment_methods" => array(
+    array("id" => "amex")
+  ),
+  "excluded_payment_types" => array(
+    array("id" => "atm")
+  ),
+  "installments" => 6
+);
 
-	$preference->save();
+	$preference->back_urls = array(
+                        "success" => "$basedir/success.php",
+                        "failure" => "$basedir/failure.php",
+                        "pending" => "$basedir/pending.php"
+                    );
+	 $preference->external_reference = "ddistel@jotafi.com.ar";
+	 $preference->notification_url = "https://ddistel-mp-commerce-php.herokuapp.com/webhook.php"
+		
+	$payer = new MercadoPago\Payer();
+	$payer->name = "Lalo Landa";
+	$payer->email = "test_user_63274575@testuser.com";
+	
+	$payer->phone = array(
+                        "area_code" => "11",
+                        "number" => "22223333"
+                    );
+	$payer->address = array(
+                        "zip_code" => "111",
+                        "street_name" => "False",
+                       "street_number" => "123"
+                    );
+	
+	$preference->payer = $payer;	
+			
+         $preference->save();
